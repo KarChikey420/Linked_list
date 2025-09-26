3d05b0727eaa9e6a80cb5957223e62fbef7274b9
KarChikey <kartikeynegi2002@gmail.com>
2025-09-26 14:24:46 +0530

    position

--- insertion_at_middle.py
+++ insertion_at_middle.py
@@ -1,32 +1,49 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, x):
+        self.val = x
         self.next = None
 
-    @staticmethod
-    def insert_at_middle(head, data, position):
-        new_node = Node(data)
-        current = head
-        while current is not None and current.data != position:
-            current = current.next
-        if current is None:
-            # Position not found, do not insert
-            print("Position not found")
-            return head
-        new_node.next = current.next
-        current.next = new_node
+def insertPos(head, pos, val):
+
+    if pos < 1:
         return head
 
-node =Node(10)
-node1=Node(20)
-node2=Node(30)
-node3=Node(40)
+    if pos == 1:
+        newNode = Node(val)
+        newNode.next = head
+        return newNode
 
-node.next=node1
-node1.next=node2
-node2.next=node3
-node3.next=None
+    curr = head
 
-head=node
-head=Node.insert_at_middle(head,25,20)
+    for i in range(1, pos - 1):
+        if curr is None:
+            return head
+        curr = curr.next
 
+    if curr is None:
+        return head
+
+    newNode = Node(val)
+
+    newNode.next = curr.next
+    curr.next = newNode
+
+    return head
+
+def printList(head):
+    curr = head
+    while curr:
+        print(curr.val, end="")
+        if curr.next:
+            print(" -> ", end="")
+        curr = curr.next
+    print()
+
+if __name__ == "__main__":
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(4)
+    
+    val, pos = 3, 3
+    head = insertPos(head, pos, val)
+    printList(head)
