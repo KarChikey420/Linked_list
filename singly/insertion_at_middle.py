4abe20e7bb14c79b70aff3698dbb77c726ab6ba7
KarChikey <kartikeynegi2002@gmail.com>
2025-10-19 18:30:32 +0530

    ad

--- singly/insertion_at_middle.py
+++ singly/insertion_at_middle.py
@@ -1,49 +1,29 @@
 class Node:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-def insertPos(head, pos, val):
-
-    if pos < 1:
-        return head
-
-    if pos == 1:
-        newNode = Node(val)
-        newNode.next = head
-        return newNode
-
-    curr = head
-
-    for i in range(1, pos - 1):
-        if curr is None:
-            return head
-        curr = curr.next
-
-    if curr is None:
-        return head
-
-    newNode = Node(val)
-
-    newNode.next = curr.next
-    curr.next = newNode
-
-    return head
-
-def printList(head):
-    curr = head
-    while curr:
-        print(curr.val, end="")
-        if curr.next:
-            print(" -> ", end="")
-        curr = curr.next
-    print()
-
-if __name__ == "__main__":
-    head = Node(1)
-    head.next = Node(2)
-    head.next.next = Node(4)
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+        
+def insert_at_pos(head,data,index):
+    new_node=Node(data)
+    current=head
     
-    val, pos = 3, 3
-    head = insertPos(head, pos, val)
-    printList(head)
+    for i in range(1,index-1):
+        current=current.next
+    
+    new_node.next=current.next
+    current.next=new_node
+    return head
+    
+def print_list(head):
+    current=head
+    while current!=None:
+        print(current.data,end="->")
+        current=current.next
+
+if __name__=="__main__":
+    head=Node(10)
+    head.next=Node(20)
+    head.next.next=Node(30)
+    
+    head=insert_at_pos(head,25,2)
+    print_list(head)
