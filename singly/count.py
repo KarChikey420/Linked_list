4ace65a02935857286b0b3e5c3b5a1469326820a
KarChikey <kartikeynegi2002@gmail.com>
2025-10-21 21:54:42 +0530

    add

--- singly/count.py
+++ singly/count.py
@@ -2,19 +2,22 @@ class Node:
     def __init__(self,data):
         self.data=data
         self.next=None
-        
-def count_nodes(head):
-        count=0
-        curr=head
-        while curr!=None:
-            curr=curr.next
-            count+=1
-        return count
+
+def count_element(head):
+   count=0
+   current=head
+   while current!=None:
+       current=current.next
+       count+=1
+   return count
     
 if __name__=="__main__":
-    head=Node(1)
-    head.next=Node(2)
-    head.next.next=Node(3)
+    head=Node(10)
+    head.next=Node(20)
+    head.next.next=Node(30)
+    head.next.next.next=Node(40)
+    head=count_element(head)
+    print(head)
     
-    count=count_nodes(head)
-    print(count)
+    
+    
