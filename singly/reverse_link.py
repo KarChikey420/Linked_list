3179d75abc02706c583a4ec9d5246ffe707f03c8
KarChikey <kartikeynegi2002@gmail.com>
2025-10-25 10:16:11 +0530

    add\

--- singly/reverse_link.py
+++ singly/reverse_link.py
@@ -2,29 +2,38 @@ class Node:
     def __init__(self,data):
         self.data=data
         self.next=None
-
-def reverse(head):
+    
+def reverse_link(head):
     previous=None
     current=head
+    
     while current!=None:
-        next_val=current.next
+        next_node=current.next
         current.next=previous
         previous=current
-        current=next_val
+        current=next_node
     return previous
     
 def print_list(head):
     current=head
-    while current != None:
+    while current!=None:
         print(current.data,end="->")
         current=current.next
-        
+
 if __name__=="__main__":
     head=Node(10)
     head.next=Node(20)
     head.next.next=Node(30)
     head.next.next.next=Node(40)
-    head=reverse(head)
-    print_list(head)
+    head.next.next.next.next=Node(50)
     
+    reverse=reverse_link(head)
+    print_list(reverse)
+
+
+
+
+
+
+
     
