e29477a8d53de46febef57883548a31a3d96f14a
KarChikey <kartikeynegi2002@gmail.com>
2025-11-02 15:52:56 +0530

    add

--- singly/middle.py
+++ singly/middle.py
@@ -2,11 +2,10 @@ class Node:
     def __init__(self,data):
         self.data=data
         self.next=None
-    
-def middle_node(head):
+
+def find_middle(head):
     slow=head
     fast=head
-    
     while fast!=None and fast.next!=None:
         slow=slow.next
         fast=fast.next.next
@@ -25,14 +24,7 @@ if __name__=="__main__":
     head.next.next.next=Node(40)
     head.next.next.next.next=Node(50)
     
-    mid=middle_node(head)
-    print("middle node",mid.data)
+    mid=find_middle(head)
+    print("middle node:",mid.data)
 
-
-
-
-
-
-
-
-    
+   
